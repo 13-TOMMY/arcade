45ef555dd96bed0918f18053c171e46c57859d69
Tomasz Lis <125186340+13-TOMMY@users.noreply.github.com>
2023-12-07 20:39:00 +0100

    testing slack bot

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
-import BannerCard from '../components/BannerCard'
+import React from "react";
+import BannerCard from "../components/BannerCard";
+import Join from "../components/Join";
 
 function Homepage() {
   return (
     <div>
       <BannerCard />
+      <Join />
     </div>
-  )
+  );
 }
 
-export default Homepage
+export default Homepage;
