bc4af28c5c89ff900c58cba5581af5067849e421
WJStephenson <willjosteph@gmail.com>
2023-12-07 13:52:56 +0000

    state for search input in header, formatted scss

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,26 @@
 import { Link } from "react-router-dom";
 import { HiMagnifyingGlass } from "react-icons/hi2";
+import { useState } from "react";
 
 
 function Header() {
+
+  const [search, setSearch] = useState('')
+
+  const handleSearchChange = (event) => {
+    setSearch(event.target.value);
+  };
+
   return (
     <header className='header-container'>
       <Link to="/">arcade.js</Link>
       <div className="search-container">
-        <input type="text" placeholder="Search by name, genre, or creator" />
+        <input
+          type="text"
+          placeholder="Search by name, genre, or creator"
+          value={search}
+          onChange={handleSearchChange}
+        />
         <HiMagnifyingGlass className='search-icon' />
       </div>
       <div className="buttons-container">
